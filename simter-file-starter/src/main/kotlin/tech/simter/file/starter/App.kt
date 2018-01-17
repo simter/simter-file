8e124588578e59b30b0c94d7d006631ad827d570
RJ.Hwang <rongjihuang@gmail.com>
2018-01-17 16:35:38 +0800

    Polishing

--- simter-file-starter/src/main/kotlin/tech/simter/file/starter/App.kt
+++ simter-file-starter/src/main/kotlin/tech/simter/file/starter/App.kt
@@ -3,7 +3,7 @@ package tech.simter.file.starter
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["tech.simter"])
 class App
 
 fun main(args: Array<String>) {
