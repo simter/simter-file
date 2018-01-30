f2c9ab21e9f8f739cdf168ea740b3a305afc8a94
RJ.Hwang <rongjihuang@gmail.com>
2018-01-30 17:22:50 +0800

    Implements Dao.findById by jpa

--- simter-file-data-jpa/src/main/kotlin/tech/simter/file/dao/jpa/AttachmentJpaDao.kt
+++ simter-file-data-jpa/src/main/kotlin/tech/simter/file/dao/jpa/AttachmentJpaDao.kt
@@ -1,15 +1,34 @@
 package tech.simter.file.dao.jpa
 
+import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.Repository
 import tech.simter.file.po.Attachment
+import java.util.*
 
 /**
- * The block JPA-DAO Repository.
+ * The block JPA-DAO Repository. See [CrudRepository].
  *
  * @author RJ
  */
 //@RepositoryDefinition(domainClass = Attachment::class, idClass = String::class)
 interface AttachmentJpaDao : Repository<Attachment, String> {
-  /** Create a new Attachment */
-  fun save(entity: Attachment): Attachment
+  /**
+   * Create a new Attachment.
+   *
+   * See [CrudRepository.save].
+   *
+   * @param attachment the attachment
+   * @return the saved attachment
+   */
+  fun save(attachment: Attachment): Attachment
+
+  /**
+   * Find an Attachment by its id.
+   *
+   * See [CrudRepository.findById].
+   *
+   * @param id the identity
+   * @return the attachment with the given id or [Optional.empty] if none found
+   */
+  fun findById(id: String): Optional<Attachment>
 }
