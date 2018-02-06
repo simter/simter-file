77eae7ccf21ea1bd8c91e8b5d9b2741b73077a55
Smart-Jie <1650481457@qq.com>
2018-02-06 17:49:26 +0800

    Implement WebFlux file-view

--- simter-file-rest-webflux/src/main/kotlin/tech/simter/file/rest/webflux/router/RouterConfiguration.kt
+++ simter-file-rest-webflux/src/main/kotlin/tech/simter/file/rest/webflux/router/RouterConfiguration.kt
@@ -7,8 +7,10 @@ import org.springframework.web.reactive.function.server.*
 import org.springframework.web.reactive.function.server.RequestPredicates.*
 import reactor.core.publisher.Mono
 import tech.simter.file.rest.webflux.handler.DownloadFileHandler
+import tech.simter.file.rest.webflux.handler.FileViewHandler
 import tech.simter.file.rest.webflux.handler.SystemInfoHandler
 import tech.simter.file.rest.webflux.handler.UploadFileHandler
+
 
 /**
  * All routers.configuration
@@ -19,11 +21,12 @@ import tech.simter.file.rest.webflux.handler.UploadFileHandler
 class RouterConfiguration @Autowired constructor(
   private val systemInfoHandler: SystemInfoHandler,
   private val uploadFileHandler: UploadFileHandler,
-  private val downloadFileHandler: DownloadFileHandler
+  private val downloadFileHandler: DownloadFileHandler,
+  private val fileViewHandler: FileViewHandler
 ) : RouterFunction<ServerResponse> {
   override fun route(request: ServerRequest): Mono<HandlerFunction<ServerResponse>> {
-    return RouterFunctions.route(GET("/"), systemInfoHandler)   // /root
-      .andRoute(GET("/system-info"), systemInfoHandler)         // /system-info
+    return RouterFunctions.route(GET("/"), fileViewHandler)   // /?page-no=:pageNo&page-size=:pageSize
+      .andRoute(GET("/system-info"), systemInfoHandler)       // /system-info
       .andRoute(POST("/").and(contentType(MediaType.MULTIPART_FORM_DATA)), uploadFileHandler) // /root
       .andRoute(GET("/{id}"), downloadFileHandler) // /{id}
       .route(request)
---
+++ simter-file-rest-webflux/src/test/java/tech/simter/file/rest/webflux/handler/FileViewHandlerTest.java
@@ -0,0 +1,74 @@
+package tech.simter.file.rest.webflux.handler;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import tech.simter.file.po.Attachment;
+import tech.simter.file.rest.webflux.WebFluxConfiguration;
+import tech.simter.file.service.AttachmentService;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test FileViewHandler.
+ *
+ * @author JF
+ */
+@SpringJUnitConfig(classes = {
+  WebFluxConfiguration.class, FileViewHandler.class
+})
+@MockBean(AttachmentService.class)
+class FileViewHandlerTest {
+  private WebTestClient client;
+  @SpyBean
+  private FileViewHandler handler;
+  @Autowired
+  private AttachmentService service;
+
+  @BeforeEach
+  void setUp() {
+    client = WebTestClient.bindToRouterFunction(handler.router()).build();
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void fileView() throws IOException {
+    // mock
+    int pageNo = 1;
+    int pageSize = 25;
+    String id = UUID.randomUUID().toString();
+    List<Attachment> list = new ArrayList<>();
+    list.add(new Attachment(id, "/path", "name", "ext", 100, OffsetDateTime.now(), "Simter"));
+    Pageable pageable = PageRequest.of(pageNo, pageSize);
+    when(service.find(pageable)).thenReturn(Mono.just(new PageImpl(list, pageable, list.size())));
+
+    // invoke
+    client.get().uri("/?page-no=1&page-size=25")
+      .exchange()
+      .expectStatus().isOk()
+      .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+      .expectBody()
+      .jsonPath("$.pageable.pageNumber").isEqualTo(pageNo) // verify page-no
+      .jsonPath("$.pageable.pageSize").isEqualTo(pageSize) // verify page-size
+      .jsonPath("$.content[0].id").isEqualTo(id);          // verify Attachment.id
+
+    // verify
+    verify(service).find(pageable);
+  }
+}
