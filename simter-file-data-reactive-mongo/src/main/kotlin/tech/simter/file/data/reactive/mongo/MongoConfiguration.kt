35a605cc9d68a411c0c5d8ed62b7cc70244a0f94
BallsperCat <13430280541@163.com>
2018-02-06 13:46:58 +0800

    Simplify mongodb configuration

--- simter-file-data-reactive-mongo/src/main/kotlin/tech/simter/file/data/reactive/mongo/MongoConfiguration.kt
+++ simter-file-data-reactive-mongo/src/main/kotlin/tech/simter/file/data/reactive/mongo/MongoConfiguration.kt
@@ -3,11 +3,9 @@ package tech.simter.file.data.reactive.mongo
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.core.convert.converter.Converter
 import org.springframework.data.convert.CustomConversions
-import org.springframework.data.mongodb.config.MongoConfigurationSupport
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions
 import org.springframework.data.mongodb.core.mapping.event.LoggingEventListener
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
@@ -22,35 +20,21 @@ import java.util.*
  */
 @Configuration
 @EnableReactiveMongoRepositories("tech.simter")
-class MongoConfiguration : MongoConfigurationSupport() {
+class MongoConfiguration {
   @Bean
   @ConditionalOnProperty(name = ["simter.mongodb.logging-event-listener"], havingValue = "true")
   fun mongoEventListener(): LoggingEventListener {
     return LoggingEventListener()
   }
 
-  override fun getDatabaseName(): String {
-    return "test"
-  }
-
   /**
    * Add converters to custom conversions list
    */
-  override fun customConversions(): CustomConversions {
+  @Bean
+  @Primary
+  fun customConversions(): CustomConversions {
     var converterList = ArrayList<Converter<*, *>>()
     converterList.add(MongoDocumentToOffsetDateTimeConverter())
     return MongoCustomConversions(converterList)
-  }
-
-  /**
-   * Registering the converters
-   */
-  @Bean
-  @Throws(Exception::class)
-  fun mappingMongoConverter(): MappingMongoConverter {
-    val converter = MappingMongoConverter(ReactiveMongoTemplate.NO_OP_REF_RESOLVER,
-      mongoMappingContext())
-    converter.setCustomConversions(customConversions())
-    return converter
   }
 }
