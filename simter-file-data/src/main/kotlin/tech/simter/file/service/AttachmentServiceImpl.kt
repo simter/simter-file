9e72f38e6124dc03bfe70858137df1ce15cd8ab8
BallsperCat <13430280541@163.com>
2018-02-07 10:54:29 +0800

    Design backend interface for file view

--- simter-file-data/src/main/kotlin/tech/simter/file/service/AttachmentServiceImpl.kt
+++ simter-file-data/src/main/kotlin/tech/simter/file/service/AttachmentServiceImpl.kt
@@ -1,6 +1,8 @@
 package tech.simter.file.service
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import tech.simter.file.dao.AttachmentDao
@@ -16,6 +18,10 @@ import tech.simter.file.po.Attachment
 class AttachmentServiceImpl @Autowired constructor(val attachmentDao: AttachmentDao) : AttachmentService {
   override fun get(id: String): Mono<Attachment> {
     return attachmentDao.findById(id)
+  }
+
+  override fun find(pageable: Pageable): Mono<Page<Attachment>> {
+    return attachmentDao.findAll(pageable)
   }
 
   override fun create(attachment: Mono<Attachment>): Mono<Attachment> {
