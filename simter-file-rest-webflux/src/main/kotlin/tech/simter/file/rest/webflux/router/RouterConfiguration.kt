5ea79e9a66c8f9eace6d7e894e0eb4a13b375f4e
Smart-Jie <1650481457@qq.com>
2018-02-11 14:01:55 +0800

    Implement WebFlux file-form

--- simter-file-rest-webflux/src/main/kotlin/tech/simter/file/rest/webflux/router/RouterConfiguration.kt
+++ simter-file-rest-webflux/src/main/kotlin/tech/simter/file/rest/webflux/router/RouterConfiguration.kt
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.*
 import org.springframework.web.reactive.function.server.RequestPredicates.*
 import reactor.core.publisher.Mono
-import tech.simter.file.rest.webflux.handler.DownloadFileHandler
-import tech.simter.file.rest.webflux.handler.FileViewHandler
-import tech.simter.file.rest.webflux.handler.SystemInfoHandler
-import tech.simter.file.rest.webflux.handler.UploadFileHandler
+import tech.simter.file.rest.webflux.handler.*
 
 
 /**
@@ -22,13 +19,15 @@ class RouterConfiguration @Autowired constructor(
   private val systemInfoHandler: SystemInfoHandler,
   private val uploadFileHandler: UploadFileHandler,
   private val downloadFileHandler: DownloadFileHandler,
-  private val fileViewHandler: FileViewHandler
+  private val fileViewHandler: FileViewHandler,
+  private val fileFormHandler: FileFormHandler
 ) : RouterFunction<ServerResponse> {
   override fun route(request: ServerRequest): Mono<HandlerFunction<ServerResponse>> {
     return RouterFunctions.route(GET("/"), systemInfoHandler) // /root
       .andRoute(GET("/system-info"), systemInfoHandler)       // /system-info
       .andRoute(POST("/").and(contentType(MediaType.MULTIPART_FORM_DATA)), uploadFileHandler) // /root
-      .andRoute(GET("/attachment"), fileViewHandler) // /attachment?page-no=:pageNo&page-size=:pageSize
+      .andRoute(GET("/attachment"), fileViewHandler)      // /attachment?page-no=:pageNo&page-size=:pageSize
+      .andRoute(GET("/attachment/{id}"), fileFormHandler) // /attachment/{id}
       .andRoute(GET("/{id}"), downloadFileHandler)   // /{id}
       .route(request)
   }
---
+++ simter-file-rest-webflux/src/test/java/tech/simter/file/rest/webflux/handler/FileFormHandlerTest.java
@@ -0,0 +1,62 @@
+package tech.simter.file.rest.webflux.handler;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import tech.simter.file.po.Attachment;
+import tech.simter.file.rest.webflux.WebFluxConfiguration;
+import tech.simter.file.service.AttachmentService;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test FileFormHandler.
+ *
+ * @author JF
+ */
+@SpringJUnitConfig(classes = {
+  WebFluxConfiguration.class, FileFormHandler.class
+})
+@MockBean(AttachmentService.class)
+class FileFormHandlerTest {
+  private WebTestClient client;
+  @SpyBean
+  private FileFormHandler handler;
+  @Autowired
+  private AttachmentService service;
+
+  @BeforeEach
+  void setUp() {
+    client = WebTestClient.bindToRouterFunction(handler.router()).build();
+  }
+
+  @Test
+  void fileForm() throws IOException {
+    // mock
+    String id = UUID.randomUUID().toString();
+    Attachment attachment = new Attachment(id, "/path", "name", "ext", 100,
+      OffsetDateTime.now(), "Simter");
+    when(service.get(id)).thenReturn(Mono.just(attachment));
+
+    // invoke
+    client.get().uri("/attachment/" + id)
+      .exchange()
+      .expectStatus().isOk()
+      .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+      .expectBody().jsonPath("$.id").isEqualTo(id);
+
+    // verify
+    verify(service).get(id);
+  }
+}
