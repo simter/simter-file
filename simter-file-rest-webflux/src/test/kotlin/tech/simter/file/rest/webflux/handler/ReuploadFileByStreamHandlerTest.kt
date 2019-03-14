475763fddf10f747e65865db0934be014f902f68
yapozhong <879728937@qq.com>
2019-03-14 11:30:36 +0800

    Modify ReuploadFileByStreamHandler add to deal with forbidden

--- simter-file-rest-webflux/src/test/kotlin/tech/simter/file/rest/webflux/handler/ReuploadFileByStreamHandlerTest.kt
+++ simter-file-rest-webflux/src/test/kotlin/tech/simter/file/rest/webflux/handler/ReuploadFileByStreamHandlerTest.kt
@@ -1,5 +1,6 @@
 package tech.simter.file.rest.webflux.handler
 
+import com.nhaarman.mockito_kotlin.any
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
@@ -14,6 +15,7 @@ import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.function.server.RouterFunctions.route
 import reactor.core.publisher.Mono
 import tech.simter.exception.NotFoundException
+import tech.simter.exception.PermissionDeniedException
 import tech.simter.file.dto.AttachmentDto
 import tech.simter.file.rest.webflux.handler.ReuploadFileByStreamHandler.Companion.REQUEST_PREDICATE
 import tech.simter.file.service.AttachmentService
@@ -35,7 +37,7 @@ internal class ReuploadFileByStreamHandlerTest @Autowired constructor(
   private val client = bindToRouterFunction(route(REQUEST_PREDICATE, handler)).build()
 
   @Test
-  fun uploadByNofileName() {
+  fun `Reupload by no file name`() {
     // mock
     val file = ClassPathResource("logback-test.xml")
     val fileData = file.file.readBytes()
@@ -60,7 +62,7 @@ internal class ReuploadFileByStreamHandlerTest @Autowired constructor(
   }
 
   @Test
-  fun reuploadByHasfileName() {
+  fun `Reupload by has file name`() {
     // mock
     val name = "logback-test"
     val ext = "xml"
@@ -117,5 +119,29 @@ internal class ReuploadFileByStreamHandlerTest @Autowired constructor(
 
     // verify
     verify(service).reuploadFile(attachment, fileData)
+  }
+
+  @Test
+  fun `Failed by permission denied`() {
+    // mock
+    val name = "logback-test"
+    val ext = "xml"
+    val file = ClassPathResource("$name.$ext")
+    val fileData = file.file.readBytes()
+    val id = UUID.randomUUID().toString()
+    val fileSize = file.contentLength()
+    `when`(service.reuploadFile(any(), any())).thenReturn(Mono.error(PermissionDeniedException()))
+
+    // invoke
+    client.patch().uri("/$id")
+      .header("Content-Disposition", "attachment; name=\"filedata\"; filename=\"$name.$ext\"")
+      .contentType(MediaType.APPLICATION_OCTET_STREAM)
+      .contentLength(fileSize)
+      .syncBody(fileData)
+      .exchange()
+      .expectStatus().isForbidden
+
+    // verify
+    verify(service).reuploadFile(any(), any())
   }
 }
