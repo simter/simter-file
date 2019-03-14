f672c2d43977efafe26c20f4ce695e6e16c708b8
yapozhong <879728937@qq.com>
2019-03-14 10:49:07 +0800

    Modify DeleteFilesHandler add to deal with forbidden

--- simter-file-rest-webflux/src/main/kotlin/tech/simter/file/rest/webflux/handler/DeleteFilesHandler.kt
+++ simter-file-rest-webflux/src/main/kotlin/tech/simter/file/rest/webflux/handler/DeleteFilesHandler.kt
@@ -9,6 +9,11 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 import tech.simter.file.service.AttachmentService
+import org.springframework.http.HttpStatus.FORBIDDEN
+import org.springframework.web.reactive.function.server.ServerResponse.status
+import tech.simter.exception.ForbiddenException
+import tech.simter.exception.PermissionDeniedException
+import tech.simter.reactive.web.Utils.TEXT_PLAIN_UTF8
 
 /**
  * The [HandlerFunction] for delete files.
@@ -19,15 +24,22 @@ import tech.simter.file.service.AttachmentService
  * Delete {context-path}/{ids}
  * ```
  *
- * Response:
+ * Response: (if deleted)
  *
  * ```
  * 204 No Content
  * ```
  *
+ * Response: (if permission denied or across module)
+ *
+ * ```
+ * 403 Forbidden
+ * ```
+ *
  * [More](https://github.com/simter/simter-file/wiki/Delete-Files)
  *
  * @author JW
+ * @author zh
  */
 @Component
 class DeleteFilesHandler @Autowired constructor(
@@ -36,6 +48,14 @@ class DeleteFilesHandler @Autowired constructor(
   override fun handle(request: ServerRequest): Mono<ServerResponse> {
     return attachmentService.delete(*request.pathVariable("ids").split(",").toTypedArray())
       .then(ServerResponse.noContent().build())
+      .onErrorResume(PermissionDeniedException::class.java) {
+        if (it.message.isNullOrEmpty()) status(FORBIDDEN).build()
+        else status(FORBIDDEN).contentType(TEXT_PLAIN_UTF8).syncBody(it.message!!)
+      }
+      .onErrorResume(ForbiddenException::class.java) {
+        if (it.message.isNullOrEmpty()) status(FORBIDDEN).build()
+        else status(FORBIDDEN).contentType(TEXT_PLAIN_UTF8).syncBody(it.message!!)
+      }
   }
 
   companion object {
