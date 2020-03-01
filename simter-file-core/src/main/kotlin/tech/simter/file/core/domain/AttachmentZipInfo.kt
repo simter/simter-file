25dd048eb9756e6b63532689491c66d7ab000647
RJ <rongjihuang@gmail.com>
2020-03-01 10:11:40 +0800

    Remove javax.persistence-api dependency on core module

--- simter-file-core/src/main/kotlin/tech/simter/file/core/domain/AttachmentZipInfo.kt
+++ simter-file-core/src/main/kotlin/tech/simter/file/core/domain/AttachmentZipInfo.kt
@@ -1,14 +1,11 @@
 package tech.simter.file.core.domain
 
-import javax.persistence.Id
-
 interface AttachmentZipInfo {
   /**
    * the origin is nullable, so cannot set terminus and origin to joint the primary key
    * if origin is null, id = "null-$terminus"
    * if origin is not null, id = "\"$origin\"-\"$terminus\""
    */
-  @get:Id
   val id: String
   val type: String
   /** descendant's id  */
